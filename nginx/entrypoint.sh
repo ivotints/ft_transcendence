895ea0d15e93ea03e304a270bca5ef4a76b4a3e7
0xSuitQ <tosnikcorporation@gmail.com>
2024-10-19 13:18:42 +0200

    added nginx as a reverse-proxy

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+    -subj "/C=CZ/L=PR/O=42/OU=cadet/CN=localhost"
+
+echo "
+server {
+    listen 443 ssl;
+    listen [::]:443 ssl;
+
+    server_name localhost;
+
+    ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+
+    ssl_protocols TLSv1.3;
+
+    location / {
+        proxy_pass http://frontend:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}" > /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;"
