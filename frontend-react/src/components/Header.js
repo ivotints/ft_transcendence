4a3d1fac67bc859dc1748099347c8e240cd04cc3
ivotints <ivotints@ivotints42.myguest.virtualbox.org>
2024-10-23 23:42:29 +0200

    Added translation to header. Works bad. After chouse of language you need to click on any of the header buttons.

--- frontend-react/src/components/Header.js
+++ frontend-react/src/components/Header.js
@@ -1,38 +1,62 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-//import { useTranslation } from 'react-i18next'; // Import useTranslation hook
 import './Header.css';
+
+var Language = "en";
 
 function Header() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  //const { t, i18n } = useTranslation(); // Destructure t (translate function) and i18n instance
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
-  const changeLanguage = (language) => {
-    //i18n.changeLanguage(language); // Change the language
-    //setDropdownOpen(false); // Close the dropdown after changing the language
+  const changeLanguage = (lang) => {
+      Language = lang;
   };
+
+  const translate = (word) => {
+    if (Language == 'ru')
+    {
+      if (word == 'Home')
+        return ("Дом");
+      if (word == "Profile")
+        return ("Профиль");
+      if (word == "Game")
+        return ("Игра");
+      if (word == "Tournament")
+        return ("Турнир");
+      if (word == "Language")
+        return ("Язык");
+    };
+    if (Language == 'cz')
+      {
+        if (word == 'Home')
+          return ("Dům");
+        if (word == "Profile")
+          return ("Profil");
+        if (word == "Game")
+          return ("Hra");
+        if (word == "Tournament")
+          return ("Turnaj");
+        if (word == "Language")
+          return ("Jazyk");
+      };
+    return (word);
+};
 
   return (
     <header className="header">
       <nav className="nav">
         <ul className="nav-list">
-{/*           <li className="nav-item"><Link to="/">{t('home')}</Link></li>
-          <li className="nav-item"><Link to="/profile">{t('profile')}</Link></li>
-          <li className="nav-item"><Link to="/game">{t('game')}</Link></li>
-          <li className="nav-item"><Link to="/tournament">{t('tournament')}</Link></li> */}
-          <li className="nav-item"><Link to="/">Home</Link></li>
-          <li className="nav-item"><Link to="/profile">Profile</Link></li>
-          <li className="nav-item"><Link to="/game">Game</Link></li>
-          <li className="nav-item"><Link to="/tournament">Tournament</Link></li>
+          <li className="nav-item"><Link to="/">{translate('Home')}</Link></li>
+          <li className="nav-item"><Link to="/profile">{translate('Profile')}</Link></li>
+          <li className="nav-item"><Link to="/game">{translate('Game')}</Link></li>
+          <li className="nav-item"><Link to="/tournament">{translate('Tournament')}</Link></li>
         </ul>
         <div className="language-dropdown">
           <button className="dropdown-button" onClick={toggleDropdown}>
-          {/* {t('language')} */}
-          <b>language</b>
+          <b>{translate('Language')}</b>
           </button>
           {dropdownOpen && (
             <ul className="dropdown-menu">
