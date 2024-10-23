86fa56ffd1c49efaa52c6395a4f968ae077693da
ivotints <ivotints@ivotints42.myguest.virtualbox.org>
2024-10-23 22:33:13 +0200

    I am Igor

--- frontend-react/src/components/Header.js
+++ frontend-react/src/components/Header.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+//import { useTranslation } from 'react-i18next'; // Import useTranslation hook
 import './Header.css';
 
 function Header() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  //const { t, i18n } = useTranslation(); // Destructure t (translate function) and i18n instance
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const changeLanguage = (language) => {
+    //i18n.changeLanguage(language); // Change the language
+    //setDropdownOpen(false); // Close the dropdown after changing the language
+  };
+
   return (
     <header className="header">
       <nav className="nav">
         <ul className="nav-list">
+{/*           <li className="nav-item"><Link to="/">{t('home')}</Link></li>
+          <li className="nav-item"><Link to="/profile">{t('profile')}</Link></li>
+          <li className="nav-item"><Link to="/game">{t('game')}</Link></li>
+          <li className="nav-item"><Link to="/tournament">{t('tournament')}</Link></li> */}
           <li className="nav-item"><Link to="/">Home</Link></li>
           <li className="nav-item"><Link to="/profile">Profile</Link></li>
           <li className="nav-item"><Link to="/game">Game</Link></li>
           <li className="nav-item"><Link to="/tournament">Tournament</Link></li>
         </ul>
+        <div className="language-dropdown">
+          <button className="dropdown-button" onClick={toggleDropdown}>
+          {/* {t('language')} */}
+          <b>language</b>
+          </button>
+          {dropdownOpen && (
+            <ul className="dropdown-menu">
+              <li><button onClick={() => changeLanguage('en')}>EN</button></li>
+              <li><button onClick={() => changeLanguage('ru')}>RU</button></li>
+              <li><button onClick={() => changeLanguage('cz')}>CZ</button></li>
+            </ul>
+          )}
+        </div>
       </nav>
     </header>
   );
 }
 
 export default Header;
+
